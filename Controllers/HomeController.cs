5cd0179f73ca5bf7bed22e234279ab035a853256
Dennis Kim <ddkim4@gmail.com>
2020-10-13 16:52:33 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AS_DojoSurveyModel.Models;
+
+namespace AS_DojoSurveyModel.Controllers
+{
+    public class HomeController : Controller
+    {
+    
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("Result")] ///this is the route?
+        public IActionResult PostMethod(User newUser)
+        {
+            return View("Result", newUser);
+        }
+        
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace AS_DojoSurveyModel.Models
+{
+    public class User
+    {
+        public string Name {get;set;}
+        public string Email {get;set;}
+        public string Password {get;set;}
+        public string Location {get;set;}
+        public string Language {get;set;}
+        public string Comment {get;set;}
+    }
+        
+}
